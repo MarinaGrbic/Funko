c999bee0b4fa96ad6bf38358a74fc4b602a6b7c3
MarinaGrbic <grbic.marina@yahoo.com>
2021-04-02 16:11:35 +0200

    wwww

--- podaci.js
+++ podaci.js
@@ -3,7 +3,7 @@ var podaci = [
         id : 1,
         naziv : "Guns N' Roses",
         cena: 3000,
-        slika : "../../Slike/GNRFunko.jpg",  
+        slika : "GNRFunko.jpg"  
         
     },
 
